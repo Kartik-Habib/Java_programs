2cf4c9d0703307529c1e8c21d10477c3dedfa4fd
Kartik Habib <habibkartik2014@gmail.com>
2022-11-23 23:00:22 +0530

    Adding the example for map().

--- src/com/jav8/StreamAPI.java
+++ src/com/jav8/StreamAPI.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class StreamAPI {
     public static void main(String[] args) {
         // Basic examples
-       List<Integer> numbers= List.of(12,5,8,4,2,6,3,8,10);
-
+        List<Integer> numbers= List.of(12,5,8,4,2,6,3,8,10);
+        //Using filer
         System.out.println("Printing the even numbers");
         numbers.stream().filter(num->num%2==0).forEach(num-> System.out.print(num +" "));
 
         System.out.println("\nPrinting the odd numbers");
         numbers.stream().filter(num->num%2!=0).forEach(num-> System.out.print(num +" "));
 
+        //Using map
+        System.out.println("\nPrinting the Square of numbers");
+        numbers.stream().map(x->x*x).forEach(x-> System.out.print(x +" "));
 
 
     }
